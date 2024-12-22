4eef91f5fe30803e8260db63bd8f2a5910562596
Marcelly Guimarães <souzaguimaraesarcelly@gmail.com>
2024-12-22 15:43:21 -0300

    add dados corretamente

--- src/main/java/br/com/isissouzaguimaraes/desafio_todolist/entity/Todo.java
+++ src/main/java/br/com/isissouzaguimaraes/desafio_todolist/entity/Todo.java
@@ -18,21 +18,6 @@ public class Todo {
     private boolean realizado;
     private int prioridade;
 
-    public Todo(Long id, String nome, String descricao, boolean realizado, int prioridade) {
-        this.id = id;
-        this.nome = nome;
-        this.descricao = descricao;
-        this.realizado = realizado;
-        this.prioridade = prioridade;
-    }
-
-    public Todo(String nome, String descricao, boolean realizado, int prioridade) {
-        this.nome = nome;
-        this.descricao = descricao;
-        this.realizado = realizado;
-        this.prioridade = prioridade;
-    }
-
     public Long getId() {
         return id;
     }
